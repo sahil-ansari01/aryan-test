fd22f4a4ef42540ea3a823cc5f0303470c41ce5b
Sahil Ansari <opensourcecarrier@gmail.com>
2023-03-14 10:31:34 +0530

    auth domain

--- src/lib/firebase/init.jsx
+++ src/lib/firebase/init.jsx
@@ -7,7 +7,7 @@ import { initializeApp, getApps } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
   projectId: process.env.NEXT_PUBLIC_projectId,
   storageBucket: process.env.NEXT_PUBLIC_storageBucket,
   messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
